24a33ee6b0d00242bc95c86d2c191cbf10df95fb
xavier <xavierhoskins@outlook.com>
2020-05-24 20:49:15 -0500

    added tables and database

---
+++ database/migrations/20200524195135_user.js
@@ -0,0 +1,28 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('users', (users) => {
+      users.increments();
+      users.string('first_name');
+      users.string('last_name');
+      users.string('username', 20).notNullable().unique();
+      users.string('password', 20).notNullable();
+      users.integer('phone', 11).notNullable();
+    })
+
+    .createTable('plants', (plants) => {
+      plants.increments();
+      plants.string('name', 50).notNullable();
+      plants.string('species', 50).notNullable();
+      plants.string('water_freq', 50).notNullable();
+      plants
+        .integer('user_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('users');
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('users').dropTableIfExists('plants');
+};
